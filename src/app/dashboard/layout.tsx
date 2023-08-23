d4eb7f2299c2de29e6e497b2ba9b437b4dfd94f0
Seth Torrence <slionel1850@gmail.com>
2023-08-23 22:46:23 +0000

    Attempt Number 3

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -5,12 +5,12 @@ import Navbar from '@/components/nav/Navbar'
 
 export default function DashboardLayout({ children }: LayoutProps) {
     return (
-        <section className='min-h-screen'>
+        <div className='min-h-screen'>
             <Navbar />
-            <div className='h-full'>
+            <div className='min-h-[90vh]'>
                 {children}
             </div>
             <Footer />
-        </section>
+        </div>
     )
 }
