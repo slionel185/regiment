10359bab44ba9da73e1c259d9fee6f70715b2b2f
Seth Torrence <slionel1850@gmail.com>
2023-08-25 03:06:45 +0000

    Fix Border on Home Page

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,8 +1,17 @@
+import { getServerSession } from 'next-auth'
+
+import { authOptions } from '@/utils/auth'
+
 export default async function DashboardPage() {
+    const session = await getServerSession(authOptions)
+    console.log(session)
 
     return (
-        <div>
-            
+        <div className='flex flex-col h-full justify-center items-center'>
+            <div className='flex flex-col'>
+                <h1 className='text-primary text-3xl lg:text-5xl font-bold'>Welcome<span className='text-foreground'>,</span></h1>
+                <p>{session?.user.username}</p>
+            </div>
         </div>
     )
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -13,7 +13,7 @@ export default async function HomePage() {
 
     return (
         <div className='h-screen flex flex-col justify-center items-center px-4'>
-            <Card className='w-full px:4 border-none max-w-xl relative shadow'>
+            <Card className='w-full px:4 border-none sm:border-solid max-w-xl relative shadow'>
                 <div className='absolute top-4 right-4'>
                     <RedirectButton to='/auth/login'>Login</RedirectButton>
                 </div>
